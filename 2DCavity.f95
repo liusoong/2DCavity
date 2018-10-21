9d060dd246abdecf4ee30554a2b2f0f55f4312d2
liusoong <song.liu.19831229@gmail.com>
2018-10-21 20:41:04 +0800

    debug

--- 2DCavity.f95
+++ 2DCavity.f95
@@ -11,10 +11,10 @@ program twoDCavity
 	real(8) 				:: u(0 : n, 0 : n + 1), v(0 : n + 1, 0 : n)
 	real(8)				:: p(n, n)
 	real(8) 				:: h, dt	
-	integer				:: i
+	integer				:: i, j
 
 	h = 1.0 / n
-	dt = 0.01 * h * h * Re
+	dt = 0.04 * h * h * Re
 	
 	u = 0.0
 	v = 0.0
@@ -27,13 +27,29 @@ program twoDCavity
 		!~ print *, p(n, n)
 		!printData(u, v, p)
 		if (mod(i, 100) == 0) then
-			print *, u((n-1)/2, n/2), v(n/2, (n-1)/2), p(n/2, n/2)
+			print *, i, u((n-1)/2, n/2), v(n/2, (n-1)/2), p(n/2, n/2)
 		end if
 	end do
 	
 	!~ do i = 1, n
 		!~ print *, i, p(i, i)	!!!!!!!!!!!!!!!!!!!!!!!!!
 	!~ end do
+	
+		
+	! print out result
+	open (unit = 1, file = "u")
+	open (unit = 2, file = "v")	
+	open (unit = 3, file = "p")
+	do i = 1, n
+		write(1, "(f15.8, 5x, f15.8)") (i-0.5) * h, u(n / 2, i)
+		write(2, "(f15.8, 5x, f15.8)") (i-0.5) * h, v(i, n / 2)		
+		do j = 1, n
+			write(3, "(f15.8, 5x, f15.8, 5x, f15.8)") (i-0.5) * h, (j-0.5) * h, p(i, j)		
+		end do
+	end do
+	close(1)
+	close(2)
+	close(3)
 	
 end program twoDCavity
 
--- moduleParameters.f95
+++ moduleParameters.f95
@@ -2,8 +2,8 @@ module moduleParameters
 
 	implicit none
 	
-	integer, parameter 	:: n = 64
-	integer, parameter 	:: maxIt = 100000
+	integer, parameter 	:: n = 256
+	integer, parameter 	:: maxIt = 200000
 	real(8), parameter 	:: Re = 10000.0
 	real(8), parameter 	:: drivingV = 1.0
 	
